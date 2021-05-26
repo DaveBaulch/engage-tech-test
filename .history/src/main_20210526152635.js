95ee47be41d19c9c10d99f0a8bd014fdc414810e
Dave Baulch <davebaulch@yahoo.co.uk>
2021-05-26 15:28:28 +0100

    moved directive back into main to resolve initial load issue

---
+++ .history/src/main_20210526152635.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import router from "./router";
+import store from "./store";
+import App from "./App.vue";
+import VueMeta from "vue-meta";
+// import AnimationDirective from "./directives/animations";
+import gsap from "gsap";
+import "./tailwind.css";
+// Vue.directive("animate", AnimationDirective);
+Vue.config.productionTip = false;
+
+Vue.use(VueMeta);
+
+new Vue({
+  router,
+  store,
+  created() {
+    const html = document.documentElement; // returns the html tag
+    html.setAttribute("lang", "en");
+  },
+  render: (h) => h(App),
+}).$mount("#app");
